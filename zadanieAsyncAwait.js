1d169a093c5ce2d51249e56d63ba5f30171f7f61
Koelmer <core.safecrlf>
2022-05-13 12:30:12 +0200

    Change to promise

--- zadanieAsyncAwait.js
+++ zadanieAsyncAwait.js
@@ -2,28 +2,22 @@ const API_URL = 'https://randomuser.me/api/?inc=gender,first,name,nat,location,r
 
 async function getData(){
     try{
-        const response = new Promise((resolve,reject) => {
-            const data = fetch(API_URL).then(response => response.json()).then(data => console.log(data)).catch(err => alert(err));
-            if(!data){
-                reject('Error in fetch')
-            }
-            else resolve(data);
-        })
-        if(!response){
-            throw new Error('Error in fetch')
-        }
-        console.log(response)
-        return response;
-    }catch(err){
-        if(err instanceof Error){
-            console.log(`Connection error `)
-        }
+        const data = await fetchData();
+    }  catch(err){
         console.log(`There is an error ${err}`)
     } 
 }
+const fetchData = () => {
+    return new Promise((resolve,reject) => {
+        const fetched = fetch(API_URL).then((response) => response.json()).then(data => console.log(data))
+        if(!fetched){
+            reject("Error in fetch")
+        }
+        resolve(fetched)
+    })
+}
 
 getData();
-
 
 
 
